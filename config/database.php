01c969977e984f3283f3e69d3fc2f2a428164cdb
RosaleeCastro <rosalee.castrot@gmail.com>
2026-03-19 10:11:22 +0100

    conexion bbdd ticketpro_db

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+$host ="localhost";
+$dbname ="ticketpro_db";
+$user = "root";
+$pass = "";
+
+  //Crear la conexión con MySQL usando mysqli 
+  
+  $conexion = new mysqli($host, $user, $pass, $dbname);
+
+  //verificar si hubo error de conexión
+
+  if($conexion->connect_error){
+    die("Error de conexion : ". $conexion->connect_error);
+  }
+
+  // Establecer el juego de caracteres a utf8mb4
+  $conexion->set_charset("utf8mb4");
+?>
